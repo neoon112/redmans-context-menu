ae319256e2bb657dc7b1129b23456dffcef589c1
neoon <69065710+neoon112@users.noreply.github.com>
2024-09-27 18:51:29 +1000

    Update Renameto.ps1

--- Renameto.ps1
+++ Renameto.ps1
@@ -1,35 +1,29 @@
 $filePath = $args[0]
-$suffix = $args[1]  # Get the second argument for custom suffix
+$suffix = $args[1]
 
-# Set a log file path for debugging
-$logFile = "C:\Users\TomasStather\script_log.txt"
-Start-Transcript -Path $logFile
+# Everything that says #uncomment you can uncomment for a log file to debug if this does not work
+
+#$logFile = "C:\Apps\RenameTo\script_log.txt" # uncomment
+#Start-Transcript -Path $logFile # uncomment
 
 try {
-    # First, check if the file path is valid and not empty
     if (-not [string]::IsNullOrWhiteSpace($filePath) -and -not [string]::IsNullOrWhiteSpace($suffix)) {
         
-        # Trim and sanitize the file path
         $filePath = $filePath.Trim()
 
-        # Split the directory and the filename
         $directory = Split-Path -Parent $filePath
         $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
         $extension = [System.IO.Path]::GetExtension($filePath)
 
-        # Log the individual parts
         Write-Host "Directory: $directory"
         Write-Host "File Name without Extension: $fileNameWithoutExtension"
         Write-Host "File Extension: $extension"
 
-        # Construct the new file name with the custom suffix
         $newFileName = "$fileNameWithoutExtension$suffix$extension"
         $newFilePath = Join-Path $directory $newFileName
 
-        # Log the new file path
         Write-Host "New file path: $newFilePath"
 
-        # Rename the file
         Rename-Item -Path $filePath -NewName $newFilePath
     } else {
         Write-Host "Invalid file path or suffix received."
@@ -38,4 +32,4 @@ try {
     Write-Error $_
 }
 
-Stop-Transcript
+#Stop-Transcript #uncomment
