caaab94eaa19bb427fd2b42a6219fe84a8c9e88a
neoon <69065710+neoon112@users.noreply.github.com>
2024-10-01 15:09:52 +1000

    Update SendTo.ps1

--- SendTo.ps1
+++ SendTo.ps1
@@ -14,3 +14,14 @@ $body = @{
 $response = Invoke-RestMethod -Uri $url -Method Post -Body ($body | ConvertTo-Json) -ContentType "application/json"
 
 $response
+
+Add-Type -AssemblyName System.Windows.Forms
+
+if ($response -eq "")
+{
+    [System.Windows.Forms.MessageBox]::Show("Successfully uploaded!", "Result", 'OK', 'Information')
+}
+else
+{
+    [System.Windows.Forms.MessageBox]::Show("$response", "Error", 'OK', 'Error')   
+}
